2ee000863a94ed87e38b52a9bf707584196af0a8
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-05-14 19:10:07 -0500

    Update

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(
   express.urlencoded({
     extended: true,
+    limit: '50mb'
   })
 );
 
