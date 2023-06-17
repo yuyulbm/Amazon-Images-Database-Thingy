6404bb577d49ddfd1956003c86cfa2ded8aa17ab
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-06-17 15:01:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '50mb'
+  })
+);
 
 app.get("/", (_, res) => res.redirect("https://discord.gg/j3YamACwPu"));
 
